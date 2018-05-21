4600c378b76917f0505074d8549f1b68df59a2ef
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-05-21 14:48:38 +0530

    Update p21.py

--- p21.py
+++ p21.py
@@ -1,7 +1,9 @@
 (x1,y1)=(0,2)
 (x2,y2)=(1,6)
 (x3,y3)=(2,10)
-if((y2-y1)/(x2-x1)==(y3-y1)/(x3-x1)):
+try:
+   ((y2-y1)/(x2-x1)==(y3-y1)/(x3-x1))
+except ZeroDivisionError:
    print((x1,y1),(x2,y2),(x3,y3),"are in same laine")
 else:
    print((x1,y1),(x2,y2),(x3,y3),"are not in same laine")
