e481d46720ea4a8ddd1b74d6878c209b94c6f79e
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-05-21 14:44:28 +0530

    Create p21.py

---
+++ p21.py
@@ -0,0 +1,8 @@
+(x1,y1)=(0,2)
+(x2,y2)=(1,6)
+(x3,y3)=(2,10)
+if((y2-y1)/(x2-x1)==(y3-y1)/(x3-x1)):
+   print((x1,y1),(x2,y2),(x3,y3),"are in same laine")
+else:
+   print((x1,y1),(x2,y2),(x3,y3),"are not in same laine")
+
